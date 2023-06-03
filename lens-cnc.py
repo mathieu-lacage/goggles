b34d6950fa7a609bbebebf84e3ba9b6187bbd448
Mathieu Lacage <mathieu.lacage@alcmeon.com>
2023-06-03 11:27:21 +0200

    rework lens for cnc

---
+++ lens-cnc.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+import math
+
+import solid
+import solid.utils
+
+import utils
+import constants
+import lens
+
+
+def myopia_correction(diopters, x_offset=0, y_offset=0):
+    if diopters is None:
+        return None
+    assert diopters <= 4
+    n2 = 1.49 # PMMA
+    n1 = 1 # air
+    # lens maker equation applied to a plano-concave lens
+    R1 = (n2/n1 - 1) / diopters
+    # convert meters to millimeters
+    R1 = R1*1000
+
+    half_width = max(constants.ELLIPSIS_WIDTH + math.fabs(x_offset), constants.ELLIPSIS_HEIGHT + math.fabs(y_offset)) - constants.SKIRT_THICKNESS
+    delta = math.sqrt(R1**2-half_width**2)
+    o = solid.sphere(r=R1, segments=500)
+    o = solid.translate([x_offset, y_offset, delta])(o)
+    #o = solid.debug(o)
+    return o
+
+
+def lens_cnc(correction=None):
+    o = solid.cube([constants.ELLIPSIS_WIDTH*2, constants.ELLIPSIS_HEIGHT*2, constants.LENS_HEIGHT], center=True)
+    o = solid.translate([0, 0, -constants.LENS_HEIGHT/2])(o)
+    if correction is not None:
+        tmp = utils.ring(100, -constants.LENS_BOTTOM_RING_WIDTH)
+        tmp = solid.translate([0, 0, -50])(tmp)
+        x = solid.intersection()([tmp, correction])
+        o = o - x
+    return o
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--resolution', default=40, type=int)
+    parser.add_argument('-e', '--export', action='store_true')
+    parser.add_argument('--slice-x', default=None, type=float)
+    parser.add_argument('--slice-y', default=None, type=float)
+    parser.add_argument('--slice-z', default=None, type=float)
+    parser.add_argument('--slice-a', default=None, type=float)
+    parser.add_argument('--diopters', default=None, type=float)
+    parser.add_argument('--x-offset', default=0, type=float)
+    parser.add_argument('--y-offset', default=0, type=float)
+    args = parser.parse_args()
+
+    constants.NSTEPS = args.resolution
+    correction = myopia_correction(diopters=args.diopters, x_offset=args.x_offset, y_offset=args.y_offset)
+    l = lens_cnc(correction=correction)
+
+    if args.slice_a is not None or args.slice_x is not None or args.slice_y is not None or args.slice_z is not None:
+        cut = utils.slice(args)
+        l = l - cut
+    solid.scad_render_to_file(l, 'lens-cnc.scad')
+
+    if args.export:
+        utils.export('lens-cnc', 'stl', args.resolution)
+
+
+if __name__ == '__main__':
+    main()
--- lens.py
+++ lens.py
@@ -5,59 +5,12 @@ import solid
 import solid.utils
 
 import utils
+import mg2
+import ggg
 import constants
 
 
-def myopia_correction(diopters, x_offset=0, y_offset=0):
-    if diopters is None:
-        return None
-    assert diopters <= 4
-    n2 = 1.49 # PMMA
-    n1 = 1 # air
-    # lens maker equation applied to a plano-concave lens
-    R1 = (n2/n1 - 1) / diopters
-    # convert meters to millimeters
-    R1 = R1*1000
-
-    max_half_width = max(constants.ELLIPSIS_WIDTH, constants.ELLIPSIS_HEIGHT)-constants.SKIRT_THICKNESS+max(math.fabs(x_offset), math.fabs(y_offset))
-    delta = math.sqrt(R1**2-max_half_width**2)
-    o = solid.sphere(r=R1, segments=1000)
-    o = solid.translate([x_offset, y_offset, -delta])(o)
-#    o = solid.debug(o)
-    return o
-
-
-def lens_top(delta=0):
-    epsilon = 0.01
-    m2 = utils.ring(constants.LENS_GROOVE_HEIGHT+epsilon*2, -constants.LENS_GROOVE_DEPTH-constants.SKIRT_THICKNESS+delta)
-    top = utils.ring(constants.LENS_TOP_HEIGHT, -constants.SKIRT_THICKNESS+delta)
-
-    l = solid.translate([0, 0, 0])(m2) + \
-        solid.translate([0, 0, constants.LENS_GROOVE_HEIGHT])(top)
-
-    l = solid.mirror([0, 0, 1])(l)
-    l = solid.translate([0, 0, 0])(l)
-    l = solid.color("grey", 0.7)(l)
-    return l
-
-
-def lens_bottom(correction=None, delta=0):
-#    print(2*(constants.ELLIPSIS_WIDTH-constants.SKIRT_THICKNESS), 2*(constants.ELLIPSIS_HEIGHT-constants.SKIRT_THICKNESS))
-    epsilon = 0.01
-    bottom = utils.ring(constants.LENS_BOTTOM_RING_HEIGHT, constants.LENS_BOTTOM_RING_WIDTH*2/3)
-    m1 = utils.ring(constants.SHELL_THICKNESS+constants.SKIRT_SQUASHED_THICKNESS+epsilon, -constants.SKIRT_THICKNESS+delta)
-    l = bottom
-    l = l + solid.translate([0, 0, constants.LENS_BOTTOM_RING_HEIGHT-epsilon])(m1)
-    if correction is not None:
-        l = l - correction
-
-    l = solid.mirror([0, 0, 1])(l)
-    l = solid.translate([0, 0, 0])(l)
-    l = solid.color("grey", 0.7)(l)
-    return l
-
-
-def lens(correction=None):
+def lens():
     #
     #                A------------------
     #                |      top
@@ -81,13 +34,29 @@ def lens(correction=None):
     #  GH=constants.LENS_BOTTOM_RING_HEIGHT (1)
     #  Total height: 1+1+1.2+0.4+1=4.6 ??
 
-    bot = lens_bottom(correction=correction)
-    top = lens_top()
+    PADDING = 1
+    # start at E
+    shape = mg2.Path(x=-constants.SKIRT_THICKNESS, y=0) \
+        .append(dy=constants.SHELL_THICKNESS+constants.SKIRT_THICKNESS)\
+        .append(dx=constants.LENS_BOTTOM_RING_WIDTH)\
+        .append(dy=constants.LENS_BOTTOM_RING_HEIGHT)\
+        .append(dx=-constants.LENS_BOTTOM_RING_WIDTH-constants.LENS_GROOVE_DEPTH-PADDING)\
+        .append(dy=-constants.LENS_HEIGHT)\
+        .append(dx=PADDING+constants.LENS_GROOVE_DEPTH)\
+        .append(dy=constants.LENS_TOP_HEIGHT)\
+        .append(dx=-constants.LENS_GROOVE_DEPTH)\
+        .append(dy=constants.LENS_GROOVE_HEIGHT)
 
-    l = bot
-    l = l + solid.translate([0, 0, -(constants.LENS_BOTTOM_RING_HEIGHT+constants.SHELL_THICKNESS+constants.SKIRT_SQUASHED_THICKNESS)])(top)
-    l = solid.translate([0, 0, constants.LENS_BOTTOM_RING_HEIGHT+constants.SKIRT_SQUASHED_THICKNESS])(l)
-    return l
+    path = utils.ellipsis_path()
+    o = ggg.extrude(list(shape.points)).along_closed_path(path).mesh().solidify()
+    epsilon = 0
+    tmp = utils.ring(constants.LENS_HEIGHT+epsilon*2, -(constants.SKIRT_THICKNESS+constants.LENS_GROOVE_DEPTH+PADDING)+0.3)
+    tmp = solid.translate([0, 0, -(constants.LENS_TOP_HEIGHT+constants.LENS_GROOVE_HEIGHT)-epsilon])(tmp)
+    o = o + tmp
+    offset = constants.LENS_HEIGHT-(constants.LENS_TOP_HEIGHT+constants.LENS_GROOVE_HEIGHT)
+    o = solid.translate([0, 0, -offset])(o)
+    o = solid.translate([0, 0, offset-constants.SHELL_THICKNESS])(o)
+    return o
 
 
 def generate_lens_svg():
@@ -105,15 +74,8 @@ def generate_lens_svg():
         context.restore()
 
         context.save()
+        context.save()
         context.scale(constants.ELLIPSIS_WIDTH+delta1, constants.ELLIPSIS_HEIGHT+delta1)
-        context.arc(0, 0, 1, 0, 2*math.pi)
-        context.set_source_rgba(0.5, 0.5, 0.5, 1)
-        context.fill()
-        context.restore()
-
-        context.save()
-        context.save()
-        context.scale(constants.ELLIPSIS_WIDTH+delta1+kerf/2, constants.ELLIPSIS_HEIGHT+delta1+kerf/2)
         context.arc(0, 0, 1, 0, 2*math.pi)
         context.restore()
         context.set_source_rgba(1, 0, 0, 1)
@@ -122,10 +84,13 @@ def generate_lens_svg():
         context.restore()
 
         context.save()
+        context.save()
         context.scale(constants.ELLIPSIS_WIDTH+delta2, constants.ELLIPSIS_HEIGHT+delta2)
         context.arc(0, 0, 1, 0, 2*math.pi)
-        context.set_source_rgba(1, 1, 1, 1)
-        context.fill()
+        context.restore()
+        context.set_source_rgba(1, 0, 0, 1)
+        context.set_line_width(laser)
+        context.stroke()
         context.restore()
 
         context.save()
@@ -140,26 +105,37 @@ def generate_lens_svg():
         context = cairo.Context(surface)
 
         context.save()
-        context.translate(constants.ELLIPSIS_WIDTH+spacing, constants.ELLIPSIS_HEIGHT+spacing)
-        half_lens(context, 0, -constants.LENS_GROOVE_DEPTH, constants.LENS_GROOVE_HEIGHT)
-        context.restore()
-
-        context.save()
         context.translate(constants.ELLIPSIS_WIDTH*3+spacing*2, constants.ELLIPSIS_HEIGHT+spacing)
-        half_lens(context, constants.LENS_BOTTOM_RING_WIDTH*2/3, 0, constants.SHELL_THICKNESS+constants.SKIRT_SQUASHED_THICKNESS)
+        half_lens(
+            context,
+            -constants.SKIRT_THICKNESS+constants.LENS_BOTTOM_RING_WIDTH,
+            -constants.SKIRT_THICKNESS,
+            constants.LENS_HEIGHT-constants.LENS_BOTTOM_RING_HEIGHT
+        )
         context.restore()
 
 
 def lens_clip(height, width, alpha):
-    a = utils.ring(height, width)
-    b = utils.ring(height+2, -constants.LENS_GROOVE_DEPTH-constants.SKIRT_THICKNESS)
-    b = solid.translate([0, 0, -1])(b)
+    deltay = constants.LENS_GROOVE_HEIGHT+constants.LENS_TOP_HEIGHT
+    deltax = constants.SHELL_MIN_WIDTH+constants.SKIRT_THICKNESS
+    shape = mg2.Path(x=constants.SHELL_MIN_WIDTH, y=0)\
+        .append(dy=-(deltay), dx=-deltax/2)\
+        .append(dx=-(deltax/2))\
+        .splinify()\
+        .append(dy=constants.LENS_TOP_HEIGHT)\
+        .append(dx=-constants.LENS_GROOVE_DEPTH)\
+        .append(dy=constants.LENS_GROOVE_HEIGHT)
+    path = utils.ellipsis_path()
+    a = ggg.extrude(list(shape.reversed_points)).along_closed_path(path).mesh().solidify()
+    a = solid.translate([0, 0, constants.LENS_TOP_HEIGHT])(a)
+
     c = solid.cube([200, 200, 200], center=True)
     c = solid.translate([0, 100, 0])(c)
     d = solid.rotate([0, 0, 360*alpha/2/(2*math.pi)])(c)
     e = solid.rotate([0, 0, -360*alpha/2/(2*math.pi)])(c)
+    e = solid.scale([1, 1, 1.01])(e)
     f = d - e
-    o = a - b - f
+    o = a - f
     o = solid.mirror([1, 0, 0])(o)
     o = solid.translate([0, 0, -height-constants.SHELL_THICKNESS])(o)
     return o
@@ -174,14 +150,10 @@ def main():
     parser.add_argument('--slice-y', default=None, type=float)
     parser.add_argument('--slice-z', default=None, type=float)
     parser.add_argument('--slice-a', default=None, type=float)
-    parser.add_argument('--diopters', default=None, type=float)
-    parser.add_argument('--x-offset', default=0, type=float)
-    parser.add_argument('--y-offset', default=0, type=float)
     args = parser.parse_args()
 
     constants.NSTEPS = args.resolution
-    correction = myopia_correction(diopters=args.diopters, x_offset=args.x_offset, y_offset=args.y_offset)
-    l = lens(correction=correction)
+    l = lens()
     lc = lens_clip(constants.LENS_GROOVE_HEIGHT, 2, math.pi/4)
 
     assembly = l + lc
